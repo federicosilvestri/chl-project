c8a9ac22583aa09b84012c0473b50ec57d6846fc
Federico Silvestri <federico.silvestri@vedrai.com>
2022-05-28 17:03:10 +0200

    Add first python script (yuppi!)

--- src/dataset_intersection.py
+++ src/dataset_intersection.py
@@ -4,27 +4,46 @@ import pandas as pd
 
 from src.dataset_loader import load_dataset
 
-DATASET_PATH: str = "../dataset/first_disease_sel/"
-
-list_of_ds = load_dataset(DATASET_PATH)
-
 
 def normalize_colname(colname: str) -> str:
     normalized = colname.replace(' ', '')
     return normalized.upper()
 
 
-def intersect_ds(data_frames: typing.List[pd.DataFrame]) -> typing.Set[str]:
+def intersect_ds_colnames(data_frames: typing.List[pd.DataFrame]) -> typing.Set[str]:
     list_of_cols: [str] = []
     for df in data_frames:
         col_name_list = df.columns.values.tolist()
         list_of_cols.append([normalize_colname(col) for col in col_name_list])
 
-    # print(f"Length of dataset {len(set(list_of_cols))}")
     return set.intersection(*map(set, list_of_cols))
 
 
-intersection = intersect_ds(list_of_ds)
+def clean_datasets(data_frames: typing.List[pd.DataFrame], shared_columns: typing.List[str]) -> typing.List[pd.DataFrame]:
+    """
+    Remove columns that are not "shared between datasets".
+    :param data_frames: the dataframes
+    :return:
+    """
+    clean_dataset: typing.List[pd.DataFrame] = []
+    for df in data_frames:
+        df_cleaned = df[list(shared_columns)]
+        clean_dataset.append(df_cleaned)
+    return clean_dataset
 
 
-print(f"Length of intersection {len(intersection)}")
+def intersect_ds(data_frames: typing.List[pd.DataFrame]) -> pd.DataFrame:
+    big_ds = pd.concat(data_frames)
+    return big_ds
+
+
+
+DATASET_PATH: str = "../dataset/first_disease_sel/"
+# MUST BE UPPERCASE
+DISEASE_COLNAME: str = 'DISEASE'
+
+datasets = load_dataset(DATASET_PATH, DISEASE_COLNAME)
+int_colnames = intersect_ds_colnames(datasets)
+datasets = clean_datasets(datasets, int_colnames)
+BIG_DS = intersect_ds(datasets)
+
