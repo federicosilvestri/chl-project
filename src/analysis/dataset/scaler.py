fa19052f38b6fc833e19c956cb6fcfb51f27a3d8
Federico Silvestri <federico.silvestri@vedrai.com>
2022-05-31 11:58:27 +0200

    Fix scaling

--- src/analysis/dataset/scaler.py
+++ src/analysis/dataset/scaler.py
@@ -3,23 +3,15 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 
-def scale_minmax(datasets: tp.List[pd.DataFrame]) -> tp.List[pd.DataFrame]:
+def scale_minmax(datasets: tp.List[pd.DataFrame]):
     """Scale dataset using minmax.
     :param datasets the dataset to be scaled
     """
-    datasets_scaled: tp.List[pd.DataFrame] = []
     for dataset in datasets:
         mm_scaler = MinMaxScaler()
-        df = dataset.copy(deep=True)
-        # Dropping non-numeric column
-        disease_col = df['DISEASE']
-        df.drop(columns=['DISEASE'], axis=1, inplace=True)
+        columns = [col_name for col_name in dataset.columns if col_name != 'DISEASE']
+        dataset[columns] = mm_scaler.fit_transform(dataset[columns])
 
-        df_scaled: pd.DataFrame = pd.DataFrame(mm_scaler.fit_transform(df), columns=df.columns)
-        df_scaled.join(disease_col)
-        datasets_scaled.append(df_scaled)
-
-    return datasets_scaled
 
 
 
