1ab181fa44c1875483e3dc43202c9dfc7ae58665
Federico Silvestri <federico.silvestri@vedrai.com>
2022-05-31 11:23:33 +0200

    Add normalizer

--- src/analysis/dataset/__init__.py
+++ src/analysis/dataset/__init__.py
@@ -1,5 +1,6 @@
 from .loader import load_datasets
 from .merger import compute_ds_col_intersection, clean_datasets, build_dataset
+from .scaler import scale_minmax
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -8,6 +9,7 @@ logging.basicConfig(level=logging.INFO)
 __all__ = [
     "load_datasets",
     "compute_ds_col_intersection",
+    "scale_minmax",
     "clean_datasets",
     "build_dataset",
 ]
