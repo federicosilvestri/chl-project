79297e270aaf874d4338f889a31491a54d20a611
Federico Silvestri <federico.silvestri@vedrai.com>
2022-05-31 10:13:30 +0200

    Add bioinfokit

--- src/analysis/dataset/__init__.py
+++ src/analysis/dataset/__init__.py
@@ -1,5 +1,9 @@
 from .loader import load_datasets
 from .merger import compute_ds_col_intersection, clean_datasets, build_dataset
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 __all__ = [
     "load_datasets",
