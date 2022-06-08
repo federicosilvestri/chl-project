73135d6ba1823de98dea8d4e4cd79df2dc862d8f
Federico Silvestri <federico.silvestri@vedrai.com>
2022-06-08 19:01:12 +0200

    add correlation

---
+++ src/analysis/feature_selection/correlation.py
@@ -0,0 +1,12 @@
+import typing as tp
+import pandas as pd
+import scipy as sc
+
+
+def compute_correlation(df: pd.DataFrame) -> None:
+    """
+    This function is a utility function that uses correlation to perform feature selection.
+    It's an experimental method.
+    :parameter df: dataset
+    """
+
